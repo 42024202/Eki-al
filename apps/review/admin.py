4b932273177037f01ce1c1709bbb321e24fa6759
Ramzan9809 <ramzan.rgwctx9808@gmail.com>
2025-12-15 18:08:43 +0600

    edit admin.py

--- apps/review/admin.py
+++ apps/review/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 from apps.review.models import Review, ReviewImage
+from django.utils.html import format_html
 
 
+class ReviewImageInline(admin.TabularInline):
+    model = ReviewImage
+    extra = 1
+    readonly_fields = ('image_preview',)
 
-admin.site.register(Review)
-admin.site.register(ReviewImage)
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="100" height="100" />', obj.image.url)
+        return "-"
+    image_preview.short_description = "Превью"
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'user', 'rating', 'created_at', 'updated_at')
+    list_filter = ('rating', 'created_at')
+    search_fields = ('comment', 'user__username', 'product__name')
+    readonly_fields = ('created_at', 'updated_at')
+    inlines = [ReviewImageInline]
+
+
+@admin.register(ReviewImage)
+class ReviewImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'review', 'image_preview')
+    readonly_fields = ('image_preview',)
